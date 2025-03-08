f82277a0b2b5740fb58d3ea76badd59609ea2906
Me Anon <me@anon.com>
2025-03-08 18:50:41 +0100

    plism

--- N480.nix
+++ N480.nix
@@ -74,6 +74,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
+  services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "plasma";
   services.displayManager.sddm.wayland.enable = true;
 
