013126b2d7951764ff3534e1040626179a18f1db
Me Anon <me@anon.com>
2025-03-08 12:42:06 +0100

    Fixed flake

--- flake.nix
+++ flake.nix
@@ -6,36 +6,40 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = inputs@{
-    self,
-    nixpkgs,
-    ...  
-  }:{
-
-    flake = {
-
-      N480 = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-          ./hardware-configuration.nix
-          ./configs/common/configuration.nix
-          ./configs/N480/configuration.nix
-        ];
+  outputs = {self, nixpkgs, ... }:
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
       };
+      lib = nixpkgs.lib;
+    in {
 
-      nix_pavilion = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-          ./hardware-configuration.nix
-          ./configs/common/configuration.nix
-          ./configs/nix_pavilion/configuration.nix
-        ];
+      nixosConfigurations = {
+
+        N480 = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./hardware-configuration.nix
+            ./configs/common/configuration.nix
+            ./configs/N480/configuration.nix
+          ];
+        };
+
+        nix_pavilion = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./hardware-configuration.nix
+            ./configs/common/configuration.nix
+            ./configs/nix_pavilion/configuration.nix
+          ];
+        };
+
       };
 
     };
-
-  };
 
 }
