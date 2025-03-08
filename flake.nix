37f837e77a332337d9baab0f6855975512d78192
Me Anon <me@anon.com>
2025-03-08 12:55:26 +0100

    Stuf

--- flake.nix
+++ flake.nix
@@ -7,38 +7,37 @@
   };
 
   outputs = inputs@{self, nixpkgs, ... }:
-    let 
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      lib = nixpkgs.lib;
-    in {
-
-      flake = {
-        nixosConfigurations = {
-
-          N480 = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
-            system = "x86_64-linux";
-            modules = [
-              ./configs/common/configuration.nix
-              ./configs/N480/configuration.nix
-            ];
-          };
-
-          nix_pavilion = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
-            system = "x86_64-linux";
-            modules = [
-              ./configs/common/configuration.nix
-              ./configs/nix_pavilion/configuration.nix
-            ];
-          };
-
-        };
-      };
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
+    lib = nixpkgs.lib;
+  in {
+
+    nixosConfigurations = {
+
+      N480 = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        system = "x86_64-linux";
+        modules = [
+          ./configs/common/configuration.nix
+          ./configs/N480/configuration.nix
+        ];
+      };
+
+      nix_pavilion = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        system = "x86_64-linux";
+        modules = [
+          ./configs/common/configuration.nix
+          ./configs/nix_pavilion/configuration.nix
+        ];
+      };
+      
+    };
+
+  };
 
 }
