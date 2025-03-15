d81d278c8e5cb83b12afb75c99f669461976155e
Me Anon <me@anon.com>
2025-03-15 20:30:18 +0100

    Quick commit, default message

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -17,6 +17,6 @@
   programs.plasma = {
     enable = true;
     workspace.wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Path/contents/images/2560x1600.jpg";
-  }
+  };
   
 }
