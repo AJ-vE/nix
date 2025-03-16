cc278eb118d7a603a0fd27feadfe7a8d9c5ff7d9
Me Anon <me@anon.com>
2025-03-16 17:53:03 +0100

    Quick commit, default message

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -55,7 +55,7 @@
   ### COMMANDS ########################
 
   environment.shellAliases = {
-    syncrebuild = "su && cwd=$(pwd) && cd /etc/nixos && echo '\nPulling config files from git...' && git pull && echo '\nRebuilding NixOS...' && nixos-rebuild boot --flake /etc/nixos && cd $cwd && exit";
+    syncrebuild = "cwd=$(pwd) && cd /etc/nixos && echo '\nPulling config files from git...' && git pull && echo '\nRebuilding NixOS...' && sudo nixos-rebuild boot --flake /etc/nixos && cd $cwd && exit";
     syncreboot = "syncrebuild && sudo reboot now";
     quickpush = "cwd=$(pwd) && cd /etc/nixos && echo '\nPulling...' && git pull && echo '\nCommitting...' && git add . && git commit -m 'Quick commit, default message' && echo '\nPushing...' && git push && cd $cwd";
   };
