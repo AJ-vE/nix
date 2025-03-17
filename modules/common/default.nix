c152b4e9360a0a401c5081d3f468920b62b10aeb
Me Anon <me@anon.com>
2025-03-17 21:02:50 +0100

    Quick commit, default message

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -100,7 +100,6 @@
     git
     kdePackages.kate
     vscodium
-    librewolf
     obsidian
     lshw
     easyeffects
@@ -118,6 +117,40 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.librewolf;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      Preferences = {
+        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+        "cookiebanners.service.mode" = 2; # Block cookie banners
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+      };
+      ExtensionSettings = {
+        "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "tab-stash" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/tab-stash/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
 
   ### OTHER ###########################
 
