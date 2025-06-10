37ce2a62083ade80f367f1ac82462265de2d38a7
Me Anon <me@anon.com>
2025-06-10 22:29:53 +0200

    Quick commit, default message

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -102,12 +102,12 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowInsecure = true;
+  # nixpkgs.config.allowInsecure = true;
 
 
-  # nixpkgs.config.permittedInsecurePackages = [
-  #   "openssl-1.1.1w"
-  # ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
