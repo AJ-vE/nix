c2bcb905d16883c9d6f909bb8c767c2884c05bb9
Me Anon <me@anon.com>
2026-02-09 13:45:39 +0100

    Quick commit, default message

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
   description = "NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
